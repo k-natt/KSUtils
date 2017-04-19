36aae498fc2960bdaf3456bc196c9ced867debcb
Kevin Nattinger <kevin@nattinger.net>
2017-04-18 23:39:07 -0700

    New, empty project

---
+++ KSUtils/KSUtils.h
@@ -0,0 +1,19 @@
+//
+//  KSUtils.h
+//  KSUtils
+//
+//  Created by Kevin Nattinger on 4/18/17.
+//  Copyright © 2017 Kevin Nattinger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KSUtils.
+FOUNDATION_EXPORT double KSUtilsVersionNumber;
+
+//! Project version string for KSUtils.
+FOUNDATION_EXPORT const unsigned char KSUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KSUtils/PublicHeader.h>
+
+
